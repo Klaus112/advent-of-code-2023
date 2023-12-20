25faaefa829da581bd2abb8814a843ff7a998353
Klaus112 <klaus.tusch@gmail.com>
2023-12-20 13:07:34 +0100

    Day 4-2 Solution

--- day-4/main.go
+++ day-4/main.go
@@ -17,6 +17,11 @@ type Scratchcard struct {
 	myNumbers      []int
 }
 
+type computedScratchcard struct {
+	cardID              int
+	matchingNumberCount int
+}
+
 func main() {
 	f, err := os.Open(common.InputFile)
 	if err != nil {
@@ -33,9 +38,52 @@ func main() {
 		return
 	}
 
-	fmt.Println(getPointsForAllGames(cards))
+	// day 4 part 1
+	fmt.Printf("Points for all scratchcards: %d\n", getPointsForAllGames(cards))
+
+	// day 4 part 2
+	idToScratchCards := make(map[int][]computedScratchcard, len(cards))
+	for i := range cards {
+		idToScratchCards[cards[i].cardID] = []computedScratchcard{getComputedScratchCard(cards[i])}
+	}
+
+	scratchcardSum := getScratchCardCount(idToScratchCards)
+	fmt.Printf("Sum of scratchcards: %d\n", scratchcardSum)
 }
 
+// getComputedScratchCard computes the winningNumber count for each scratchcard.
+func getComputedScratchCard(card Scratchcard) computedScratchcard {
+	return computedScratchcard{
+		cardID:              card.cardID,
+		matchingNumberCount: len(common.HashIntersect[int](card.winningNumbers, card.myNumbers)),
+	}
+}
+
+// getScratchCardCount returns the count of scratchcards after the copying of winning numbers.
+//
+// Precomputing the scratchCard matchingNumbers already improved the function performance from 9.7 seconds to ~500ms.
+func getScratchCardCount(idToScratchCards map[int][]computedScratchcard) int {
+	// implements copying logik for winning numbers.
+	// cannot loop directly over the map because the execution order is random
+	for cardID := 1; cardID <= len(idToScratchCards); cardID++ {
+		cards := idToScratchCards[cardID]
+		for _, card := range cards {
+			for i := 1; i <= card.matchingNumberCount; i++ {
+				idToScratchCards[cardID+i] = append(idToScratchCards[cardID+i], idToScratchCards[cardID+i][0]) // add a single instance for every matching number of the appropriate card.
+			}
+		}
+	}
+
+	sum := 0
+	for _, cards := range idToScratchCards {
+		// fmt.Printf("CardID %d contains %d instances of the card\n", cardID, len(cards))
+		sum += len(cards)
+	}
+
+	return sum
+}
+
+// getPointsForAllGames sums up the scratchcards winning numbers for part1.
 func getPointsForAllGames(cards []Scratchcard) int {
 	res := 0
 	for _, card := range cards {
@@ -62,7 +110,7 @@ func getCards(in io.Reader) ([]Scratchcard, error) {
 		line := scanner.Text()
 		parts := strings.Split(line, ":")
 		if len(parts) != 2 {
-			return nil, fmt.Errorf("Invalid line found: %s", line)
+			return nil, fmt.Errorf("invalid line found: %s", line)
 		}
 		gameID, err := getGameID(parts[0])
 		if err != nil {
@@ -89,7 +137,7 @@ func getGameID(gameID string) (int, error) {
 		return strconv.Atoi(strings.TrimSpace(numStr))
 	}
 
-	return 0, fmt.Errorf("Invalid gameID string: %s", gameID)
+	return 0, fmt.Errorf("invalid gameID string: %s", gameID)
 }
 
 // getWinningAndMatchingNumbers from a string like this '41 48 83 86 17 | 83 86  6 31 17  9 48 53'.
